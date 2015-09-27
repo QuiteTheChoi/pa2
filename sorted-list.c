397ee9fc87670c5b913bdb2d7da917ec278c1536
Jon Getahun <Jon@nbp-132-64.nbp.ruw.rutgers.edu>
2015-09-27 13:36:23 -0400

    Updated SLInsert

--- sorted-list.c
+++ sorted-list.c
@@ -4,16 +4,9 @@
 #include <ctype.h>
 #include "sorted-list.h"
 
-SortedListPtr SLCreate(CompareFuncT cf, DestructFuncT df) {
-    SortedListPtr SL = (SortedListPtr)malloc(sizeof(SortedListPtr));
-    SL -> CompareFuncT = (CompareFuncT)malloc(sizeof(CompareFuncT));
-    SL -> CompareFuncT = cf;
-    SL -> DestructFuncT = (DestructFuncT)malloc(sizeof(DestructFuncT)); 
-    SL -> DestructFuncT = df;
-    SL -> head = (SortedListPtr)malloc(sizeof(SortedListPtr));
-    
-    return SL;
 
+SortedListPtr SLCreate(CompareFuncT cf, DestructFuncT df) {
+    
 }
 
 int SLInsert(SortedListPtr list, void *newObj) {
@@ -30,7 +23,19 @@ int SLInsert(SortedListPtr list, void *newObj) {
 
     else {
 
-       // if (
+        if (cf(list->head->data, newObj) < 0) { /*If the new object is greater than the head's data*/
+
+            SortedListPtr newHead = (SortedListPtr) malloc(sizeof(SortedListPtr));
+
+            newHead->data = newObj;
+
+            newHead->next = list->head;
+
+            list->head = newHead;
+
+        }
+
+          // list->head->next->
 
        /* SortedListPtr
 
