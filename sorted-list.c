374b30b314acd1abf264b6634e120aedee609bf3
Jon Getahun <Jon@Jons-MacBook-Air.local>
2015-09-27 21:53:21 -0400

    Updated SLInsert

--- sorted-list.c
+++ sorted-list.c
@@ -18,38 +18,94 @@ SortedListPtr SLCreate(CompareFuncT cf, DestructFuncT df) {
 int SLInsert(SortedListPtr list, void *newObj) {
 
     SortedListPtr temp = (SortedListPtr) malloc(sizeof(SortedList));
-    temp -> data = newObj;
+    temp->data = newObj;
    
     if (list->head == NULL) {
+        
         list->head = temp;
+        
         return 1;
+    
     }
 
     else {
+        
         if (list->CompareFuncT(list->head->data, newObj) < 0) { /*If the new object is greater than the head's data*/
+           
             temp->next = list->head;
             list->head = temp;
+            
             return 1;
-        }
-        else if (list->CompareFuncT(list->head->data, newObj) == 0) {  //Head and newObj is equal no need to insert data in
-            return 0;
-        }
-        else if (list->head->next == NULL) {    //This is if new obj is smaller and there is no next from head
-            list -> head -> next = temp;
-            return 1;
-        }
-
         
+        }
+        
+        else if (list->CompareFuncT(list->head->data, newObj) == 0) {  //Head and newObj is equal no need to insert data
+            
+            return 0;
+        
+        }
+       
+        else if (list->CompareFuncT(list->head->data, newObj) > 0 && list->head->next == NULL) { /*If the head's data is greater than the new object*/
 
-          // list->head->next->
+            list->head->next = temp;
 
-       /* SortedListPtr
+            return 1;
 
-        while (list->head){
+        }
 
+        else {
+            
+           // SortedListPtr curr = list->head->next;
 
-        }*/
+            SortedListPtr curr = list->head; /*Points to the head of the list*/
+
+            SortedListPtr prev = NULL; /*Initializes prev pointer*/
+
+            while (curr != NULL) { /*While the pointer is not null*/
+
+                int comp = list->CompareFuncT(curr->next->data, newObj); /*Stores the result of the comparison between curr->next's data and the new object in an int*/
+
+                prev = curr; /*Makes the previous pointer the current pointer*/
+
+                if (comp < 0) { /*If curr->next's data is less than the new object*/
+
+                    prev->next = temp; /*prev->next is temp*/
+                    temp->next = curr->next; /*temp->next is curr->next, or prev->next->next*/
+                    
+                    return 1;
+
+                }
+
+                else if (comp == 0) { /*If curr->next's data is equal to the new object*/
+
+                    return 0;
+
+                }
+
+                else { /*If curr->next's data is greater than the new object*/
+
+                    if (curr->next->next == NULL) { /*If curr->next->next is null*/
+
+                        curr->next->next = temp; /*curr->next->next is temp*/
+
+                        return 1;
+
+                    }
+
+                    else { /*If curr->next->next is not null*/
+
+                        curr = curr->next; /*Moves on to the next pointer and continues the loop*/
+
+                    }
+
+                }
+                
+            }
+
+            return 0;
+
+        }
 
     }
-    return 0;
+
 }
